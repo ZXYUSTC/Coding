6b82b2c53cc1d647413831a60a2eeebe79ae834f
dwade <dwz@mail.ustc.edu.cn>
2019-03-31 23:17:15 +0800

    Update buddlesort.cpp

--- buddlesort.cpp
+++ buddlesort.cpp
@@ -1,14 +1,21 @@
 void buddlesort(vector<int> &nums)
 {
+  bool flag = false;
   for(int i=0;i<nums.size()-1;i++)
   {
+    flag = false;
     for(int j=0;j<nums.size()-i-1;j++)
     {
       if(a[j]>a[j+1])
       {
+        flag = true;
         int tmp = a[j];
         a[j] = a[j+1];
         a[j+1] = tmp;
+      }
+      if(flag == false)
+      {
+        break;
       }
     }
   }
